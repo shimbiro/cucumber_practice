62931acf8bbe1baf8e923c432a74116fdd98bfa6
Oiro <moses.oiro.okello@gmail.com>
2016-11-23 12:32:33 +0300

    cucumber feature steps

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,30 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
-	def create
-
-	end
 	def show
-
+		@user = User.find_by(params[:id])
 	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to users_url, notice:  'Account successfully created'
+		else
+			# notice: 'Email already exists'
+			render 'new'
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirm)
+	end
+	
+
+	# private 
+	# def user_params
+	# 	# params.require(:user).permit(:name, :email, :password, :password_confirm)
+	# 	# params.require(:user).permit(:name, :email, :password, :password_confirm)
+	# end
 		
 end
